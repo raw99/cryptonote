d2775dd62f9ebd4a131b839b7046edd5dd542361
raw99 <raw99@users.noreply.github.com>
2017-04-24 21:37:45 +0300

    Update CryptoNoteConfig.h
    small bugfix

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "latcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210132ca5d3c00698440065ee4ae40028c7a22b91ca70dcb745d1624b4b5d40a600b";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121018acf2c579d38f55fee9d4807b4c32bd37fb26a2f00079a13f1eec1ba2d84c18a";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
